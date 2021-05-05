4bb1bc7e72b1369b3448a7cb1c086f9fe5ed9d39
Krae <kraewind@gmail.com>
2021-05-05 15:15:55 -0400

    commit tree 9a11fbe8faa40ab1319bc835dbed40aa3db3bbda

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const distanceFromHqInBlocks = (n) => {
+    return Math.abs(42 - n)
+}
+
+const distanceFromHqInFeet = (b) => {
+   return (distanceFromHqInBlocks(b) * 264)
+}
+
+const distanceTravelledInFeet = (s, f) => {
+    return Math.abs(distanceFromHqInFeet(s) - distanceFromHqInFeet(f))
+}
+
+const calculatesFarePrice = (s, f) => {
+    const thing = distanceTravelledInFeet(s, f)
+
+    if (thing <= 400) {
+        return 0
+    } else if (thing < 2000) { 
+        return parseFloat((((thing * .02) - 8).toFixed(2)))
+    } else if (thing > 2000 && thing < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
